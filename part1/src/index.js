7ae16ff10b1fb41a84f8eccc9bad05ab508b249a
Greg Thomas <SilentGrig@gmail.com>
2019-08-25 17:55:57 +0100

    part1d

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,31 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const History = ({ allClicks }) => {
+  if(allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
+      button press history: {allClicks.join(' ')}
     </div>
   )
 }
 
-const Footer = () => {
+const Button = ({onClick, text}) => {
   return (
-    <div>
-      greeting app created by
-      <a href="https://github.com/mluukkai">mluukkai</a>
-    </div>
-  )
-}
-
-const App = () => {
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10}/>
-      <Footer />
-    </>
+    <button onClick={onClick}>
+      {text}
+    </button>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = props => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  }
+
+  return (
+    <div>
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text="left" />
+        <Button onClick={handleRightClick} text="right" />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
