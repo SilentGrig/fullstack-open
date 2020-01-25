bcebdb17694a91264e66d502c41cf9083c140ce8
Greg Thomas <SilentGrig@gmail.com>
2020-01-25 15:30:47 +0000

    part2b workbook

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './components/Note';
 
-const App = ({ notes }) => {
-	const rows = () => notes.map(note =>
+const App = (props) => {
+	const [notes, setNotes] = useState(props.notes);
+	const [newNote, setNewNote] = useState('');
+	const [showAll, setShowAll] = useState(true);
+
+	const addNote = (event) => {
+		event.preventDefault();
+		const newNoteObject = {
+			id: notes.length + 1,
+			content: newNote,
+			date: new Date().toISOString(),
+			important: Math.random() > 0.5
+		};
+		setNewNote('');
+		setNotes(notes.concat(newNoteObject));
+	}
+
+	const handleNoteChange = (event) => setNewNote(event.target.value);
+
+	const notesToShow = showAll
+		? notes
+		: notes.filter(note => note.important);
+
+	const rows = () => notesToShow.map(note =>
 		<Note
 			key={note.id}
 			note={note}
@@ -12,9 +34,19 @@ const App = ({ notes }) => {
 	return (
 		<div>
 			<h1>Notes</h1>
+			<button onClick={() => setShowAll(!showAll)}>
+				show {showAll ? 'important' : 'all'}
+			</button>
 			<ul>
 				{rows()}
 			</ul>
+			<form onSubmit={addNote}>
+				<input
+					value={newNote}
+					onChange={handleNoteChange}
+				/>
+				<button type="submit">save</button>
+			</form>
 		</div>
 	);
 }
